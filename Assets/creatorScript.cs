7a577fff72aa2c9ddea80037008c5b7b9de4770c
h587736 <54101886+h587736@users.noreply.github.com>
2023-04-21 15:04:36 +0200

    Upgraded button to menu
    The new menu includes contains three buttons, each buttons spawns a shape of some sort. In this particular iteration it can spawn a cube, sphere and a cylinder

--- Assets/creatorScript.cs
+++ Assets/creatorScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class creatorScript : MonoBehaviour
 {
     public GameObject cubePrefab; // Reference to the cube prefab to be instantiated
+    public GameObject cylinderPrefab; 
+    public GameObject spherePrefab; 
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +23,18 @@ public class creatorScript : MonoBehaviour
     {
         Vector3 spawnPosition = Camera.main.transform.position + Camera.main.transform.forward * 2f;
         GameObject newCube = Instantiate(cubePrefab, spawnPosition, Quaternion.identity);
+    }
+
+    public void SpawnSphere()
+    {
+        Vector3 spawnPosition = Camera.main.transform.position + Camera.main.transform.forward * 2f;
+        GameObject newSphere = Instantiate(spherePrefab, spawnPosition, Quaternion.identity);
+    }
+
+    public void SpawnCylinder()
+    {
+        Vector3 spawnPosition = Camera.main.transform.position + Camera.main.transform.forward * 2f;
+        GameObject newCylinder = Instantiate(cylinderPrefab, spawnPosition, Quaternion.identity);
     }
 
 }
