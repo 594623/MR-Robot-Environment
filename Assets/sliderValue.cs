cbc7ea09d96df43f33310c126ad9fca59cd1b70d
fred <54101886+h587736@users.noreply.github.com>
2023-04-26 13:25:11 +0200

    Added visual slider value for Offset-slider

---
+++ Assets/sliderValue.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+using System.Security.Cryptography;
+using TMPro;
+using System.Diagnostics;
+
+public class sliderValue : MonoBehaviour
+{
+    public PinchSlider PinchSliderObject;
+    public TextMeshPro TextMeshProObject;
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void Start()
+    {
+        // Get a reference to the PinchSlider component
+        PinchSliderObject = FindObjectOfType<PinchSlider>();
+        TextMeshProObject.text = PinchSliderObject.SliderValue.ToString("F1");
+        PinchSliderObject.OnValueUpdated.AddListener(OnPinchSliderValueChanged);
+    }
+
+    void OnPinchSliderValueChanged(SliderEventData eventData)
+    {
+        TextMeshProObject.text = PinchSliderObject.SliderValue.ToString("F1");
+    }
+}
