6239ff0bd04d2947ee7ed7f0eee72271ffcac5ef
krist <594623@stud.hvl.no>
2023-04-27 13:39:33 +0200

    Created a table and added a ROS subscriber

---
+++ Assets/Scripts/ROSSubscriber.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Robotics.ROSSTCPConnector;
+using RosColor = RosMessageTypes.UnityRoboticsDemo.UnityColorMsg;
+
+public class ROSSubscriber : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ROSConnection.GetOrCreateInstance().Subscribe<RosColor>("color", ColorChange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ColorChange(RosColor colorMessage)
+    {
+        gameObject.GetComponent<Renderer>().material.color = new Color32((byte)colorMessage.r, (byte)colorMessage.g, (byte)colorMessage.b, (byte)colorMessage.a);
+    }
+}
