4826e045fc92d351575701705e71546d1fe37d3d
krist <594623@stud.hvl.no>
2023-05-01 14:36:55 +0200

    Added imports to the joint subscriber

--- Assets/Scripts/ROSJointSubscriber.cs
+++ Assets/Scripts/ROSJointSubscriber.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Robotics.ROSTCPConnector;
+//using JointState = RosMessageTypes.JointState;
 
 public class ROSJointSubscriber : MonoBehaviour
 {
