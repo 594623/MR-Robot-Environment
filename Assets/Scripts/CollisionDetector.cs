dff15d020317b5f674b1000d4f138d01a53ce1fb
krist <594623@stud.hvl.no>
2023-05-02 18:00:20 +0200

    Collision detector now ignores other joints of the robot

--- Assets/Scripts/CollisionDetector.cs
+++ Assets/Scripts/CollisionDetector.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CollisionDetector : MonoBehaviour
 {
+    private string[] ignoredObjects = { "base_link_inertia", "shoulder_link", "upper_arm_link", "forearm_link", "wrist_1_link", "wrist_2_link", "wrist_3_link" };
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name.Equals("base_link_inertia")) return;
-
+        // Ignores certain Game Objects such as other joints of the Articulation Body
+        if (ignoredObjects.Contains(collision.gameObject.name)) return;
+        // Logs the collision to the console
         Debug.Log("Collision detected between " + gameObject.name + " and " + collision.gameObject.name);
     }
 }
