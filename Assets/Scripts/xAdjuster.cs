a0e29d20967bc6c7c9de9d92c6cb13495e654400
fred <54101886+h587736@users.noreply.github.com>
2023-05-04 14:55:43 +0200

    Added kinematic toggeling, minor bug fixes
    Added kinematic toggeling when gravity is toggeled. Safety measure for enviroment sliders.

--- Assets/Scripts/xAdjuster.cs
+++ Assets/Scripts/xAdjuster.cs
@@ -19,7 +19,7 @@ public class xAdjuster : MonoBehaviour
         // Get the current value of the pinch slider (between 0 and 1)
         float sliderValue = eventData.NewValue;
 
-        // Move the target object along the y-axis based on the slider value
+        // Move the target object along the x-axis based on the slider value
         Vector3 newPosition = (targetObject.position);
         newPosition.x = (sliderValue - 0.5f) * movementRange * 2.0f;
         targetObject.position = newPosition;
