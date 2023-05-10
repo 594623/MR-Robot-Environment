4042048c868499c54d739f03b1ac8d26c717f593
krist <594623@stud.hvl.no>
2023-05-10 15:50:13 +0200

    Implemented Lock Environment functionality

---
+++ Assets/Scripts/ButtonMethods.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ButtonMethods : MonoBehaviour
+{
+    public void OpenMenu(GameObject menu)
+    {
+        menu.SetActive(!menu.activeSelf);
+    }
+
+    public void LockEnvironment(GameObject imageTarget)
+    {
+        ImageTargetBehaviour script = imageTarget.GetComponent<ImageTargetBehaviour>();
+        script.enabled = !script.enabled;
+        Debug.Log(script.enabled);
+    }
+}
--- Assets/Scripts/OpenMenu.cs
+++ Assets/Scripts/OpenMenu.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class OpenMenu : MonoBehaviour
 {
+    // TODO: Use ButtonMethods.OpenMenu instead.
     public void open(GameObject obj)
     {
         obj.SetActive(!obj.activeSelf);
